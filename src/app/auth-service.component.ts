5d696dfbb4dab97697bd3390df36abf7ee5676c0
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-29 16:40:38 -0700

    Adding oidc-client and an Auth Service Component

---
+++ src/app/auth-service.component.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { CoreModule } from './core/core.module';
+
+@Injectable({providedIn: CoreModule})
+export class ServiceNameService {
+    constructor() { }
+    
+}
